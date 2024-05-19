3abe4ad2cc66740a315a51db5d78afb6e38aaab6
Yaman Aswal <yaman@appsquad.com>
2024-05-19 18:39:04 +0530

    INIT

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,13 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+    return (
+        <div>
+            <nav className={styles.nav}>
+                App Nav Graph
+            </nav>
+        </div>
+    )
+}
+
+export default AppNav
